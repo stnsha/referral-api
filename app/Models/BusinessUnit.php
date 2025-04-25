2e8e9a8078854e3d0d9500578b743fd36da6a15a
Siti Anasuha <anasuharosli@gmail.com>
2025-04-25 15:52:45 +0800

    feat: added two server in swagger

--- app/Models/BusinessUnit.php
+++ app/Models/BusinessUnit.php
@@ -14,10 +14,14 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  *     @OA\Contact(name="Digital Innovation")
  * )
  * @OA\Server(
- *     url="http://172.18.28.51:8002",
- *     description="MyHealth server"
+ *      url="http://172.18.28.51:8002",
+ *      description="MyHealth Server"
  * )
  *
+ * @OA\Server(
+ *      url="http://127.0.0.1:8000",
+ *      description="Local Server"
+ * )
  */
 //url="http://127.0.0.1:8000",
 class BusinessUnit extends Model
