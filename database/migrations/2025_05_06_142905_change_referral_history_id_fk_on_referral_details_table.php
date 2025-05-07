847c0dfe9fda6b488f35813f1ff441a1d88b11d7
sammiteh <sammiteh98@gmail.com>
2025-05-07 15:20:16 +0800

    fix: change migration to fit mariadb ver

--- database/migrations/2025_05_06_142905_change_referral_history_id_fk_on_referral_details_table.php
+++ database/migrations/2025_05_06_142905_change_referral_history_id_fk_on_referral_details_table.php
@@ -3,15 +3,16 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration {
     public function up(): void
     {
         Schema::table('referral_details', function (Blueprint $table) {
             $table->dropForeign(['referral_history_id']);
-
-            $table->renameColumn('referral_history_id', 'referral_id');
         });
+
+        DB::statement('ALTER TABLE referral_details CHANGE referral_history_id referral_id BIGINT UNSIGNED');
 
         Schema::table('referral_details', function (Blueprint $table) {
             $table->foreign('referral_id')
@@ -27,9 +28,7 @@ return new class extends Migration {
             $table->dropForeign(['referral_id']);
         });
 
-        Schema::table('referral_details', function (Blueprint $table) {
-            $table->renameColumn('referral_id', 'referral_history_id');
-        });
+        DB::statement('ALTER TABLE referral_details CHANGE referral_id referral_history_id BIGINT UNSIGNED');
 
         Schema::table('referral_details', function (Blueprint $table) {
             $table->foreign('referral_history_id')
