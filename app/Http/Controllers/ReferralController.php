e9937ce99bcacb8a4d4927ea48106dc01b0cf9ba
Siti Anasuha <anasuharosli@gmail.com>
2025-07-01 12:32:52 +0800

    fix: added break in switch for status referral

--- app/Http/Controllers/ReferralController.php
+++ app/Http/Controllers/ReferralController.php
@@ -574,6 +574,7 @@ class ReferralController extends Controller
 
     private function getStatus($ref_status)
     {
+
         /**
          * Status
          * 1 = Open
@@ -582,25 +583,26 @@ class ReferralController extends Controller
          * 4 = Closed
          */
         switch ($ref_status) {
-            case 1:
+            case '1':
                 $status = 'Open';
                 break;
 
-            case 2:
+            case '2':
                 $status = 'In Progress';
                 break;
 
-            case 3:
+            case '3':
                 $status = 'Referred';
+                break;
 
-            case 4:
+            case '4':
                 $status = 'Closed';
+                break;
 
             default:
                 $status = 'Submitted';
                 break;
         }
-
         return $status;
     }
 
