97036e488c1638e47927574fc9f355aeaa4aacf1
Chee Meng Hong <cheemeng.hong@alpropharmacy.com>
2025-11-03 15:06:37 +0800

    fix: comment db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder
         );
 
         $this->call(BusinessUnitSeeder::class);
-        $this->call(BusinessUnitFormSeeder::class);
+        // $this->call(BusinessUnitFormSeeder::class);
         // $this->call(ExternalRefereeSeeder::class);
         // $this->call(ReferralSeeder::class);
     }
